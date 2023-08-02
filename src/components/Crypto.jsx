84bb74d79eeffadce31b5541262194c4c1e899b2
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-08-02 10:05:16 +0100

    message

---
+++ src/components/Crypto.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import "../css/Crypto.css";
+
+const Crypto = () => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://api.coinranking.com/v2/coins");
+        if (!response.ok) {
+          throw new Error("Network response was not OK");
+        }
+        const data = await response.json();
+        console.log(data.data.coins);
+        setData(data.data.coins);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <>
+      <div className="price-volume">
+        <span onClick={() => setPage(true)}>Pricing List</span>
+        <span onClick={() => setPage(false)}>Volume</span>
+      </div>
+      <div className="crypto-container">
+        {data.map((x) => {
+          return (
+            <div key={x.uuid} className="crypto">
+              {page ? (
+                <>
+                  <span className="d-flex align-items-center">
+                    <img src={x.iconUrl} alt="" width="20px" />
+                    <span className="symbol-price d-flex align-items-center justify-content-between">
+                      <span className="ms-3">{x.symbol}/USDT</span>
+                      <span className="ms-5">{x.price.slice(0, 7)}</span>
+                    </span>
+                  </span>
+                  <span
+                    className={`btn ${
+                      x.change > 0 ? "btn-success" : "btn-danger"
+                    }`}
+                  >
+                    {x.change}%
+                  </span>
+                </>
+              ) : (
+                <>
+                  <span className="d-flex align-items-center">
+                    <img src={x.iconUrl} alt="" width="20px" />
+                    <span className="symbol-price d-flex align-items-center justify-content-between">
+                      <span className="ms-3">{x.symbol}/USDT</span>
+                      <span className="ms-5">{x.price.slice(0, 7)}</span>
+                    </span>
+                  </span>
+                  <span className={`btn ${"btn-primary"}`}>{x.marketCap}</span>
+                </>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Crypto;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Topbar from "../components/Topbar";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
+import Crypto from "../components/Crypto";
 
 const Home = () => {
   return (
     <div>
       <Topbar />
       <Header />
+      <Crypto />
       <Navbar />
     </div>
   );
