3f5fa64e050b5ba2a2bcc8bd8109cf08f141d44c
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-08-11 13:07:37 +0100

    message

--- src/pages/DepositPage.jsx
+++ src/pages/DepositPage.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../css/DWE.css";
 import { MdArrowBackIos } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import QRCode from "react-qr-code";
 
 const DepositPage = () => {
   const [copied, setCopied] = useState(false);
-  const text = "tfygjhskdhfuyhioejrposjfopsmpoj";
+  const [text, setText] = useState("");
+  const params = useParams();
+  console.log(params.method);
+
+  useEffect(() => {
+    const address =
+      params.method === "usdt_trc29"
+        ? "0TUQSURgy9KUcV7c9AWVMrLVr1m2nHPLEZp"
+        : params.method.trim() === "usdt_erc20"
+        ? "0x0d2B1bF4Bde9D74543303C9CB846f6d9394Dc4E1"
+        : "bc1qtjlh63059h0rxsv7c70pa5gl7h6zxyh30ek4ly";
+    setText(address);
+  }, [params.method]);
+
   const handleCopyClick = () => {
     navigator.clipboard
       .writeText(text)
@@ -31,13 +44,9 @@ const DepositPage = () => {
       <div className="bottom-space">
         <form className="deposit-form">
           <h5>DEPOSIT FORM</h5>
-          <QRCode
-            value={`https://www.google.com`}
-            size={150}
-            className="p-3 border rounded"
-          />
+          <QRCode value={text} size={150} className="p-3 border rounded" />
           <h5 className="mt-3">DEPOSIT ADDRESS</h5>
-          <div className="mb-3">{text}</div>
+          <div className="mb-3 address-text">{text}</div>
           <span className="btn btn-outline-primary" onClick={handleCopyClick}>
             {copied ? "Copied!" : "Copy to Clipboard"}
           </span>
