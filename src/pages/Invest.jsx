b4eca44469ae6f3a7b76bd7ce6a1d665a1f3901e
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-08-22 12:33:30 +0100

    message

--- src/pages/Invest.jsx
+++ src/pages/Invest.jsx
@@ -4,9 +4,11 @@ import img from "../assets/profilelogo.png";
 import { MdArrowBackIos } from "react-icons/md";
 import { Link } from "react-router-dom";
 import ScrollToTop from "../components/ScrollToTop";
+import { useParams } from "react-router-dom";
 
 const Invest = ({ user }) => {
   const [loading, setLoading] = useState(false);
+  const params = useParams();
   return (
     <div className="assets-container">
       <ScrollToTop />
@@ -38,26 +40,50 @@ const Invest = ({ user }) => {
             <img src={img} alt="" width="50px" />
             <div className="text-start ms-2">
               <span>Get USDT</span> <br />
-              <span>Lock Up Mining 01</span>
+              <span>
+                {params.plan === "lockupmining01" && "Lock Up Mining 01"}
+                {params.plan === "lockupmining02" && "Lock Up Mining 02"}
+                {params.plan === "lockupmining03" && "Lock Up Mining 03"}
+                {params.plan === "lockupmining04" && "Lock Up Mining 04"}
+                {params.plan === "lockupmining05" && "Lock Up Mining 05"}
+              </span>
             </div>
           </span>
 
           <div className="text-end">
             <small>Recent(Daily Earnings)</small>
             <br />
-            <h5 className="text-warning">0.5 ~ 0.7%</h5>
+            <h5 className="text-warning">
+              {params.plan === "lockupmining01" && "0.5 ~ 0.7%"}
+              {params.plan === "lockupmining02" && "1 ~ 1.25%"}
+              {params.plan === "lockupmining03" && "1.25 ~ 1.5%"}
+              {params.plan === "lockupmining04" && "1.25 ~ 2%"}
+              {params.plan === "lockupmining05" && "2 ~ 2.5%"}
+            </h5>
           </div>
         </div>
       </div>
       <div className="text-containers">
         <div className="d-flex align-items-center justify-content-between head">
           <span>
-            <h5>5(Days)</h5>
+            <h5>
+              {params.plan === "lockupmining01" && "5(Days)"}
+              {params.plan === "lockupmining02" && "15(Days)"}
+              {params.plan === "lockupmining03" && "30(Days)"}
+              {params.plan === "lockupmining04" && "60(Days)"}
+              {params.plan === "lockupmining05" && "90(Days)"}
+            </h5>
             <span className="text-warning">Financial Cycle</span>
           </span>
 
           <span>
-            <h5>2000 ~ 99999999</h5>
+            <h5>
+              {params.plan === "lockupmining01" && "2000 ~ 99999999"}
+              {params.plan === "lockupmining02" && "5000 ~ 99999999"}
+              {params.plan === "lockupmining03" && "20000 ~ 99999999"}
+              {params.plan === "lockupmining04" && "100000 ~ 99999999"}
+              {params.plan === "lockupmining05" && "1000000 ~ 99999999"}
+            </h5>
             <span className="text-warning">Orders in Custody</span>
           </span>
         </div>
@@ -75,7 +101,13 @@ const Invest = ({ user }) => {
         <div className="d-flex align-items-center justify-content-between head">
           <span>
             <h5>Expected Revenue</h5>
-            <span className="text-warning">50.00 ~ 3499999.96</span>
+            <span className="text-warning">
+              {params.plan === "lockupmining01" && "50.00 ~ 3499999.96"}
+              {params.plan === "lockupmining02" && "750.00 ~ 18749999.81"}
+              {params.plan === "lockupmining03" && "7500.00 ~ 44999999.55"}
+              {params.plan === "lockupmining04" && "90000.00 ~ 119999998.80"}
+              {params.plan === "lockupmining05" && "1800000.00 ~ 224999997.75"}
+            </span>
           </span>
 
           <span>
