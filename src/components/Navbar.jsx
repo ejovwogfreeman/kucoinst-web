7d9278a839dc79fcfcf5b9294b0ff80444ef7674
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-08-23 20:33:46 +0100

    message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,22 @@ import { FaHouseUser, FaChartBar } from "react-icons/fa";
 import { TbChartCandle } from "react-icons/tb";
 import { BiChat } from "react-icons/bi";
 import { BsPersonSquare } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+
+  // Check if the current route is the register or login page
+  const hideNavbar =
+    location.pathname === "/register" ||
+    location.pathname === "/login" ||
+    location.pathname === "/forgot_password" ||
+    location.pathname === "/reset_password";
+
+  if (hideNavbar) {
+    return null; // Return null to hide the navbar
+  }
+
   return (
     <nav className="navbar">
       <ul>
