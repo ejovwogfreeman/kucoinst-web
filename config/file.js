52aa054b0be6bdb2b2e5a7e6cac520963ab77440
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-10-23 15:02:07 +0100

    message

--- config/file.js
+++ config/file.js
@@ -1,46 +1,5 @@
 // const multer = require("multer");
 // const { GridFsStorage } = require("multer-gridfs-storage");
-// const Grid = require("gridfs-stream");
-// const mongoose = require("mongoose");
-
-// let gfs, gridfsBucket;
-// const conn = mongoose.connection;
-
-// conn.once("open", () => {
-//   gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
-//     bucketName: "photos",
-//   });
-//   gfs = Grid(conn.db, mongoose.mongo);
-//   gfs.collection("photos");
-// });
-
-// const storage = new GridFsStorage({
-//   url: process.env.MONGO_URI,
-//   options: {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   },
-//   file: (req, file) => {
-//     const match = ["image/png", "image/jpeg", "image/jpg", "image/webp"];
-
-//     if (match.indexOf(file.mimetype) === -1) {
-//       const filename = `${Date.now()}-image-${file.originalname}`;
-//       return filename;
-//     }
-
-//     return {
-//       bucketName: "photos",
-//       filename: `${Date.now()}-image-${file.originalname}`,
-//     };
-//   },
-// });
-
-// const upload = multer({ storage });
-
-// module.exports = { upload };
-
-// const multer = require("multer");
-// const { GridFsStorage } = require("multer-gridfs-storage");
 // const mongoose = require("mongoose");
 
 // // Connect to your MongoDB here, if you haven't already
@@ -59,7 +18,7 @@
 // });
 
 // const storage = new GridFsStorage({
-//   gfs: gfs,
+//   url: process.env.MONGO_URI, // Provide your MongoDB URL here
 //   file: (req, file) => {
 //     const match = ["image/png", "image/jpeg", "image/jpg", "image/webp"];
 
@@ -102,15 +61,20 @@ const storage = new GridFsStorage({
   url: process.env.MONGO_URI, // Provide your MongoDB URL here
   file: (req, file) => {
     const match = ["image/png", "image/jpeg", "image/jpg", "image/webp"];
+    const fileId = req.body._id; // Get the custom _id from the request body
 
     if (match.indexOf(file.mimetype) === -1) {
       const filename = `${Date.now()}-image-${file.originalname}`;
-      return filename;
+      return {
+        filename,
+        metadata: { _id: fileId }, // Set the custom _id as metadata
+      };
     }
 
     return {
       bucketName: "photos",
       filename: `${Date.now()}-image-${file.originalname}`,
+      metadata: { _id: fileId }, // Set the custom _id as metadata
     };
   },
 });
